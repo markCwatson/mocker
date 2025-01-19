314d39aab8bce600c5ca75e54705535ab593b80a
Mark Watson <markus.c.watson@gmail.com>
2025-01-19 19:11:32 -0400

    modify formatting style

--- app/main.c
+++ app/main.c
@@ -28,7 +28,8 @@
 #define STACK_SIZE (1024 * 1024)
 #define CONTAINER_ROOT "/tmp/mocker"
 
-struct child_args {
+struct child_args
+{
   char **argv;
 };
 
@@ -39,18 +40,20 @@ void cleanup_container_root(void);
 int child_function(void *arg);
 
 // Error handling function
-void handle_error(const char *msg) {
+void handle_error(const char *msg)
+{
   perror(msg);
   exit(EXIT_FAILURE);
 }
 
-void setup_container_root(void) {
+void setup_container_root(void)
+{
   LOG("Creating minimal mocker root at %s\n", CONTAINER_ROOT);
 
   // Create basic directory structure
-  const char *dirs[] = {CONTAINER_ROOT,         CONTAINER_ROOT "/bin",
+  const char *dirs[] = {CONTAINER_ROOT, CONTAINER_ROOT "/bin",
                         CONTAINER_ROOT "/proc", CONTAINER_ROOT "/sys",
-                        CONTAINER_ROOT "/dev",  NULL};
+                        CONTAINER_ROOT "/dev", NULL};
 
   // Clean up any existing mocker root
   char cmd[PATH_MAX];
@@ -58,9 +61,11 @@ void setup_container_root(void) {
   system(cmd);
 
   // Create directories
-  for (const char **dir = dirs; *dir != NULL; dir++) {
+  for (const char **dir = dirs; *dir != NULL; dir++)
+  {
     LOG("Creating directory %s\n", *dir);
-    if (mkdir(*dir, 0755) && errno != EEXIST) {
+    if (mkdir(*dir, 0755) && errno != EEXIST)
+    {
       LOG("Failed to create %s: %s\n", *dir, strerror(errno));
       handle_error("mkdir");
     }
@@ -69,15 +74,16 @@ void setup_container_root(void) {
   snprintf(cmd, sizeof(cmd),
            "cp /bin/busybox %s/bin/busybox && chmod +x %s/bin/busybox",
            CONTAINER_ROOT, CONTAINER_ROOT);
-  if (system(cmd) != 0) {
+  if (system(cmd) != 0)
+  {
     fprintf(stderr, "Failed to setup busybox!\n");
     exit(1);
   }
 
   // Create essential command symlinks
   LOG("Creating symlinks...\n");
-  const char *commands[] = {"sh",    "ls",   "ps",  "mount", "umount",
-                            "mkdir", "echo", "cat", "pwd",   NULL};
+  const char *commands[] = {"sh", "ls", "ps", "mount", "umount",
+                            "mkdir", "echo", "cat", "pwd", NULL};
 
   // Change to the bin directory for creating symlinks
   char old_pwd[PATH_MAX];
@@ -87,8 +93,10 @@ void setup_container_root(void) {
   snprintf(bin_path, sizeof(bin_path), "%s/bin", CONTAINER_ROOT);
   chdir(bin_path);
 
-  for (const char **cmd_ptr = commands; *cmd_ptr != NULL; cmd_ptr++) {
-    if (symlink("busybox", *cmd_ptr) != 0 && errno != EEXIST) {
+  for (const char **cmd_ptr = commands; *cmd_ptr != NULL; cmd_ptr++)
+  {
+    if (symlink("busybox", *cmd_ptr) != 0 && errno != EEXIST)
+    {
       LOG("Warning: Failed to create symlink for %s: %s\n", *cmd_ptr,
           strerror(errno));
     }
@@ -97,7 +105,8 @@ void setup_container_root(void) {
   chdir(old_pwd);
 
   // Mount essential filesystems
-  const struct {
+  const struct
+  {
     const char *source;
     const char *target;
     const char *type;
@@ -107,10 +116,12 @@ void setup_container_root(void) {
                 {"devtmpfs", CONTAINER_ROOT "/dev", "devtmpfs", 0},
                 {NULL, NULL, NULL, 0}};
 
-  for (int i = 0; mounts[i].source != NULL; i++) {
+  for (int i = 0; mounts[i].source != NULL; i++)
+  {
     LOG("Mounting %s at %s\n", mounts[i].source, mounts[i].target);
     if (mount(mounts[i].source, mounts[i].target, mounts[i].type,
-              mounts[i].flags, NULL) == -1) {
+              mounts[i].flags, NULL) == -1)
+    {
       LOG("Warning: Could not mount %s: %s\n", mounts[i].target,
           strerror(errno));
     }
@@ -122,16 +133,19 @@ void setup_container_root(void) {
   system(cmd);
 }
 
-void cleanup_container_root(void) {
+void cleanup_container_root(void)
+{
   LOG("Cleaning up mocker root...\n");
 
   // Unmount special filesystems in reverse order
   const char *mounts[] = {CONTAINER_ROOT "/dev", CONTAINER_ROOT "/sys",
                           CONTAINER_ROOT "/proc", NULL};
 
-  for (const char **mount_point = mounts; *mount_point != NULL; mount_point++) {
+  for (const char **mount_point = mounts; *mount_point != NULL; mount_point++)
+  {
     LOG("Unmounting %s...\n", *mount_point);
-    if (umount2(*mount_point, MNT_DETACH) != 0) {
+    if (umount2(*mount_point, MNT_DETACH) != 0)
+    {
       LOG("Warning: Failed to unmount %s: %s\n", *mount_point, strerror(errno));
     }
   }
@@ -140,30 +154,35 @@ void cleanup_container_root(void) {
   char cmd[PATH_MAX];
   snprintf(cmd, sizeof(cmd), "rm -rf %s", CONTAINER_ROOT);
   LOG("Removing mocker root directory...\n");
-  if (system(cmd) != 0) {
+  if (system(cmd) != 0)
+  {
     LOG("Warning: Failed to remove mocker root: %s\n", strerror(errno));
   }
 }
 
 // Function that runs inside the container
-int child_function(void *arg) {
+int child_function(void *arg)
+{
   struct child_args *args = (struct child_args *)arg;
 
   LOG("Setting up mocker root...\n");
   setup_container_root();
 
   LOG("Changing root...\n");
-  if (chroot(CONTAINER_ROOT) == -1) {
+  if (chroot(CONTAINER_ROOT) == -1)
+  {
     handle_error("chroot");
   }
 
-  if (chdir("/") == -1) {
+  if (chdir("/") == -1)
+  {
     handle_error("chdir");
   }
 
   char **argv = args->argv;
   LOG("Attempting to execute: %s\n", argv[3]);
-  if (execvp(argv[3], &argv[3]) == -1) {
+  if (execvp(argv[3], &argv[3]) == -1)
+  {
     LOG("execvp failed: %s\n", strerror(errno));
     handle_error("execvp");
   }
@@ -171,16 +190,19 @@ int child_function(void *arg) {
   return 0;
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   setbuf(stdout, NULL);
   setbuf(stderr, NULL);
 
-  if (argc < 4) {
+  if (argc < 4)
+  {
     fprintf(stderr, "Usage: %s run <image> <command> [args...]\n", argv[0]);
     exit(1);
   }
 
-  if (strcmp(argv[1], "run") != 0) {
+  if (strcmp(argv[1], "run") != 0)
+  {
     fprintf(stderr, "Unknown command: %s\n", argv[1]);
     exit(1);
   }
@@ -190,7 +212,8 @@ int main(int argc, char *argv[]) {
 
   // Allocate stack for child
   char *stack = malloc(STACK_SIZE);
-  if (stack == NULL) {
+  if (stack == NULL)
+  {
     handle_error("malloc");
   }
 
@@ -201,13 +224,15 @@ int main(int argc, char *argv[]) {
   pid_t pid =
       clone(child_function, stack + STACK_SIZE, clone_flags | SIGCHLD, &args);
 
-  if (pid == -1) {
+  if (pid == -1)
+  {
     handle_error("clone");
   }
 
   // Wait for child to finish
   int status;
-  if (waitpid(pid, &status, 0) == -1) {
+  if (waitpid(pid, &status, 0) == -1)
+  {
     handle_error("waitpid");
   }
 
@@ -216,9 +241,12 @@ int main(int argc, char *argv[]) {
   free(stack);
 
   // Report exit status
-  if (WIFEXITED(status)) {
+  if (WIFEXITED(status))
+  {
     LOG("Container exited with status %d\n", WEXITSTATUS(status));
-  } else if (WIFSIGNALED(status)) {
+  }
+  else if (WIFSIGNALED(status))
+  {
     LOG("Container killed by signal %d\n", WTERMSIG(status));
   }
 
